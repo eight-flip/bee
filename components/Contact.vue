5aa0c5450b75a733d53f8385fefe2c28e90019b1
Maya <melokitty.1003@gmail.com>
2019-08-02 00:45:13 +0900

    formテスト9

--- components/Contact.vue
+++ components/Contact.vue
@@ -86,7 +86,7 @@
                 <div class="field">
                   <div class="control">
                     <label class="checkbox is-fullwidth" for="希望サービス[]">
-                      <input class="input" type="checkbox" id="希望サービス[]" name="ホワイトニング申し込み" value="ホワイトニング申し込み" v-model="hopes">
+                      <input class="input" type="checkbox" name="ホワイトニング申し込み" id="希望サービス[]" value="ホワイトニング申し込み" v-model="hopes">
                       ホワイトニング申し込み
                     </label>
                   </div>
@@ -104,8 +104,8 @@
                 </div>
                 <div class="field">
                   <div class="control">
-                    <label class="checkbox is-fullwidth" for="希望サービス[]" >
-                      <input class="input" type="checkbox"  id="希望サービス[]" name="その他お問い合わせ" value="その他お問い合わせ" v-model="hopes">
+                    <label class="checkbox is-fullwidth"  >
+                      <input class="input" type="checkbox" name="その他お問い合わせ" id="希望サービス[]" value="その他お問い合わせ" v-model="hopes" >
                       その他お問い合わせ
                     </label>
                   </div>
